7974f63f2948d9376f07c70a2f4bdaa0dae2bbcc
Leo <Leo3418@users.noreply.github.com>
2018-01-27 17:29:14 -0800

    Refine formatting of code in GameDetector.java
    This is a cosmetic change.
    [skip ci]

--- src/main/java/io/github/leo3418/hbwhelper/util/GameDetector.java
+++ src/main/java/io/github/leo3418/hbwhelper/util/GameDetector.java
@@ -90,7 +90,7 @@ public class GameDetector {
 
     /**
      * If this is {@code true}, {@code GuiOpenEvent} where
-     * {@code GuiDownloadTerrain} is loaded will be ignored once.
+     * {@code GuiDownloadTerrain} is loaded will be ignored once
      */
     private boolean ignoreGuiOpenEvent;
 
@@ -147,8 +147,8 @@ public class GameDetector {
      * @see #update(TickCounterTimeUpEvent)
      */
     public void prepareToReadScoreboard(EntityJoinWorldEvent event) {
-        if (hypixelDetector.isIn() && event.getEntity() ==
-                Minecraft.getMinecraft().player) {
+        if (hypixelDetector.isIn() && event.getEntity()
+                == Minecraft.getMinecraft().player) {
             tickCounter.reset();
             tickCounter.start();
         }
