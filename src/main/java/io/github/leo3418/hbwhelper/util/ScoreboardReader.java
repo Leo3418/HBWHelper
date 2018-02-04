a5891725ddd573e2645175484ceb9988e005d2c7
Leo <Leo3418@users.noreply.github.com>
2018-02-03 16:12:34 -0800

    Improve game detection mechanism
    - Add functionality to remove formatting codes from lines on scoreboard
    - Add functionality to match lines on scoreboard with a regular
    expression, which might be used by an upcoming feature
    - Support Hypixel's brand new Bed Wars Capture Mode
    - When client is waiting for a game to start, stop reading the
    scoreboard's size (which is sometimes unreliable) and read the lines on
    the scoreboard instead
    - When client is rejoining a game, stop reading the unreliable
    scoreboard and read chat messages instead, since there is a message
    that is always sent to the client when it rejoins a game
    
    Squashed commit of the following:
    
    commit 0d3c32cc67f7b2000e9b7de401ac2b4d7276a21b
    Author: Leo <Leo3418@users.noreply.github.com>
    Date:   Sat Feb 3 15:08:04 2018 -0800
    
        Improve detection mechanism of game state
    
        - Support detection of Capture Mode
        - Stop reading unreliable scoreboard when possible, and read chat
        messages instead
        - Add formatting codes for chat message matching: because client cannot
        reproduce a section sign (`§`) in chat, this insures that the mod is
        only matching messages sent from Hypixel's mini-game plugin
    
    commit e2fd061c2a492d2cec9293002b605751ae6f46dd
    Author: Leo <Leo3418@users.noreply.github.com>
    Date:   Sat Feb 3 11:58:55 2018 -0800
    
        Add regular expression for formatting codes in Minecraft

--- src/main/java/io/github/leo3418/hbwhelper/util/ScoreboardReader.java
+++ src/main/java/io/github/leo3418/hbwhelper/util/ScoreboardReader.java
@@ -24,6 +24,7 @@ import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.scoreboard.Scoreboard;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -32,6 +33,21 @@ import java.util.Collection;
  * @author Leo
  */
 public class ScoreboardReader {
+    /**
+     * Regular expression of formatting codes in Minecraft
+     * <p>
+     * I have to use the Unicode encoding for the section sign ({@code §})
+     * because Minecraft is so mean that it does not want the player to use
+     * this sign in game to produce formatted text, even if the player is
+     * merely using it in a mod's code.
+     *
+     * @see <a href="https://minecraft.gamepedia.com/Formatting_codes"
+     *         target="_top">Formatting codes in Minecraft</a>
+     * @see <a href="https://en.wikipedia.org/wiki/Section_sign">
+     *         The section sign's information on Wikipedia</a>
+     */
+    private static final String FORMATTING_REGEX = "\u00A7[0-9a-fk-or]";
+
     /**
      * Prevents instantiation of this class.
      */
@@ -42,14 +58,19 @@ public class ScoreboardReader {
      * If a scoreboard exists, returns its title. Otherwise, returns an empty
      * string.
      *
+     * @param removeFormat whether or not to remove format from the title
      * @return the scoreboard's title or an empty string
      */
-    public static String getTitle() {
+    public static String getTitle(boolean removeFormat) {
         Scoreboard scoreboard = Minecraft.getMinecraft().world.getScoreboard();
         ScoreObjective scoreboardTitle = scoreboard
                 .getObjectiveInDisplaySlot(1);
         if (scoreboardTitle != null) {
-            return scoreboardTitle.getDisplayName();
+            String title = scoreboardTitle.getDisplayName();
+            if (removeFormat) {
+                title = title.replaceAll(FORMATTING_REGEX, "");
+            }
+            return title;
         }
         return "";
     }
@@ -69,21 +90,60 @@ public class ScoreboardReader {
      * Checks if any line on the scoreboard matches a piece of text.
      *
      * @param text the text to be matched
+     * @param removeFormat whether or not to remove format from lines on the
+     *         scoreboard
      * @return {@code true} if any line matches the text, or {@code false}
      *         otherwise
      */
-    public static boolean contains(String text) {
+    public static boolean contains(String text, boolean removeFormat) {
+        Collection<String> lines = getLines(removeFormat);
+        for (String line : lines) {
+            if (line.contains(text)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if any line on the scoreboard matches a regular expression.
+     *
+     * @param regex the regular expression to be matched
+     * @param removeFormat whether or not to remove format from lines on the
+     *         scoreboard
+     * @return {@code true} if any line matches the regular expression, or
+     *         {@code false} otherwise
+     */
+    public static boolean containsRegex(String regex, boolean removeFormat) {
+        Collection<String> lines = getLines(removeFormat);
+        for (String line : lines) {
+            if (line.matches(regex)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns a {@link Collection} of all lines on the scoreboard.
+     *
+     * @param removeFormat whether or not to remove format from the lines
+     * @return a {@link Collection} of all lines on the scoreboard.
+     */
+    private static Collection<String> getLines(boolean removeFormat) {
         Scoreboard scoreboard = Minecraft.getMinecraft().world.getScoreboard();
         Collection<Score> scores = scoreboard.getScores();
+        Collection<String> lines = new ArrayList<>(getSize());
         for (Score score : scores) {
             ScorePlayerTeam line = scoreboard
                     .getPlayersTeam(score.getPlayerName());
             String lineText = ScorePlayerTeam.formatPlayerName(line,
                     score.getPlayerName());
-            if (lineText.contains(text)) {
-                return true;
+            if (removeFormat) {
+                lineText = lineText.replaceAll(FORMATTING_REGEX, "");
             }
+            lines.add(lineText);
         }
-        return false;
+        return lines;
     }
 }
