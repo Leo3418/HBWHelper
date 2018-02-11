00f24cabcd430e3dd796c20a010539394110e501
Leo <leo3418@users.noreply.github.com>
2018-02-11 12:11:36 -0800

    Let Travis CI upload Zip archives of source code instead of source JARs
    Source JARs might mislead users when they download an artifact of this
    mod. What is more, field and method names in Minecraft source code are
    obfuscated in those JARs.
    A Zip archive of source code resolves all these problems. Also, it can
    store all files in the repository, including files like build.gradle,
    which allows people who download it build an artifact by themselves.

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-# Builds other branches
-git checkout 1.8.9
-./gradlew setupCiWorkspace
-./gradlew build
-git checkout 1.9-1.10.2
-./gradlew setupCiWorkspace
-./gradlew build
+# Builds the following branches and create source archives
+branches='1.9-1.10.2 1.8.9'
+
+for branch in $branches; do
+    git checkout $branch
+    ./gradlew setupCiWorkspace
+    ./gradlew build
+    zip_name='build/libs/src-'$branch'-'$TRAVIS_BRANCH'.zip'
+    zip -r -q $zip_name .
+    zip -d -q $zip_name ".git/*"
+    zip -d -q $zip_name ".gradle/*"
+    zip -d -q $zip_name "build/*"
+done
+
+# Delete source JARs
+cd build/libs
+find . -type f -iregex '.*-sources\.jar' -delete
+
