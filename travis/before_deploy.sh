0744a654da3bbca961636857b4302a90f2f879fe
Leo <Leo3418@users.noreply.github.com>
2018-02-11 12:38:41 -0800

    Tweak syntax and comment in before_deploy.sh
    (cherry picked from commit fe76eba)

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-# Builds the following branches and create source archives
-branches='1.9-1.10.2 1.8.9'
+# Builds the following branches and creates source archives
+branches="1.9-1.10.2 1.8.9"
 
-for branch in $branches; do
-    git checkout $branch
+for branch in ${branches}; do
+    git checkout ${branch}
     ./gradlew setupCiWorkspace
     ./gradlew build
-    zip_name='build/libs/src-'$branch'-'$TRAVIS_BRANCH'.zip'
-    zip -r -q $zip_name .
-    zip -d -q $zip_name ".git/*"
-    zip -d -q $zip_name ".gradle/*"
-    zip -d -q $zip_name "build/*"
+    zip_name='build/libs/src-'${branch}'-'${TRAVIS_BRANCH}'.zip'
+    zip -r -q ${zip_name} .
+    zip -d -q ${zip_name} ".git/*"
+    zip -d -q ${zip_name} ".gradle/*"
+    zip -d -q ${zip_name} "build/*"
 done
 
-# Delete source JARs
+# Deletes source JARs
 cd build/libs
-find . -type f -iregex '.*-sources\.jar' -delete
-
+find . -type f -iregex ".*-sources\.jar" -delete
