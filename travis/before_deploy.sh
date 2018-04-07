707588d2fa9595e6c9ccb6fd9e851386c7ecd1ec
Leo <Leo3418@users.noreply.github.com>
2018-04-07 16:07:56 -0700

    Add update checking using Forge's built-in update checker
    Minecraft Forge itself has an update checker which is used for
    notifying its users when an update is available by showing a flashing
    emerald-like icon on the "Mods" button in the main menu. Luckily,
    Minecraft Forge mods can also use this update checker. For more
    technical details, refer to
    https://mcforge.readthedocs.io/en/latest/gettingstarted/autoupdate/.
    
    To make this mod's version number compatible with the update checker, I
    had to remove the supported Minecraft client versions from the version
    number. A special thing about HBW Helper's JARs is that it may support
    more than one client version, and I had indicated what versions are
    supported by adding something like [1.11, 1.12.2] to the version number.
    This does not work with Minecraft Forge update checker, so I removed it.
    However, the supported versions of a JAR would still be indicated in
    the JAR's file name, so it should not be a big deal.
    
    (cherry picked from commit 0290dde)

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [ -f before_deploy_complete ]
+then
+    echo "Pre-deployment tasks already done"
+else
+
+# Building JAR artifact
+
 # Builds the following branches and creates source archives
 branches="1.9-1.10.2 1.8.9"
 
@@ -17,3 +24,23 @@ done
 # Deletes source JARs
 cd build/libs
 find . -type f -iregex ".*-sources\.jar" -delete
+cd ../..
+
+# Generating update JSON
+
+# Gets version tags of the latest development build and the latest release build
+export LATEST_DEV_TAG="$(curl 'https://api.github.com/repos/Leo3418/HBWHelper/releases/tags/'${TRAVIS_BRANCH} | jq -r '.tag_name')"
+export LATEST_REL_TAG="$(curl 'https://api.github.com/repos/Leo3418/HBWHelper/releases/latest' | jq -r '.tag_name')"
+
+# Removes the "v" prefix of the version tags
+export LATEST_DEV_TAG="$(echo ${LATEST_DEV_TAG:1:${#LATEST_DEV_TAG}})"
+export LATEST_REL_TAG="$(echo ${LATEST_REL_TAG:1:${#LATEST_REL_TAG}})"
+
+# Replaces placeholders in update JSON file with release information
+sed -i -e "s/<dev-ver>/${LATEST_DEV_TAG}/g" travis/pages/promotions.json
+sed -i -e "s/<rel-ver>/${LATEST_REL_TAG}/g" travis/pages/promotions.json
+
+# Marks completion of pre-deployment tasks
+touch before_deploy_complete
+
+fi
