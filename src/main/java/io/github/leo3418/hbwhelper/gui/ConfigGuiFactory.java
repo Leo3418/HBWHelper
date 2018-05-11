5295ab1098da83a1c1ff8be6e5b8798f7950c64b
Leo <Leo3418@users.noreply.github.com>
2018-05-11 13:20:04 -0700

    Fix a typo in Javadoc of ConfigGuiFactory
    (cherry picked from commit 31f1977)

--- src/main/java/io/github/leo3418/hbwhelper/gui/ConfigGuiFactory.java
+++ src/main/java/io/github/leo3418/hbwhelper/gui/ConfigGuiFactory.java
@@ -39,7 +39,7 @@ import java.util.Set;
  */
 public class ConfigGuiFactory implements IModGuiFactory {
     /**
-     * Called when instantiated to initialize with the active minecraft
+     * Called when instantiated to initialize with the active Minecraft
      * instance.
      *
      * @param minecraftInstance the instance
