d8cad6e4841bbe9563ec66babb4cd40e0b8942d4
Leo <Leo3418@users.noreply.github.com>
2020-08-24 21:12:50 -0700

    Store translate keys instead of translations for DreamMode constants

--- src/main/java/io/github/leo3418/hbwhelper/game/DreamMode.java
+++ src/main/java/io/github/leo3418/hbwhelper/game/DreamMode.java
@@ -19,13 +19,9 @@
 
 package io.github.leo3418.hbwhelper.game;
 
-import com.google.common.collect.Maps;
 import net.minecraft.client.resources.I18n;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
+import java.util.Objects;
 
 /**
  * Enumeration of Bed Wars Dream mode games on Hypixel.
@@ -38,111 +34,54 @@ public enum DreamMode {
      * current Dream mode on Hypixel, or the previous selection is no longer
      * valid
      */
-    UNSELECTED(I18n.format("hbwhelper.configGui.unselected")),
+    UNSELECTED("hbwhelper.configGui.unselected"),
     /**
      * Rush mode
      */
-    RUSH(I18n.format("hbwhelper.dream.rush")),
+    RUSH("hbwhelper.dream.rush"),
     /**
      * Ultimate mode
      */
-    ULTIMATE(I18n.format("hbwhelper.dream.ultimate")),
+    ULTIMATE("hbwhelper.dream.ultimate"),
     /**
      * Castle mode
      */
-    CASTLE(I18n.format("hbwhelper.dream.castle")),
+    CASTLE("hbwhelper.dream.castle"),
     /**
      * Lucky Blocks mode
      */
-    LUCKY_BLOCKS(I18n.format("hbwhelper.dream.luckyBlocks")),
+    LUCKY_BLOCKS("hbwhelper.dream.luckyBlocks"),
     /**
      * Voidless mode
      */
-    VOIDLESS(I18n.format("hbwhelper.dream.voidless")),
+    VOIDLESS("hbwhelper.dream.voidless"),
     /**
      * Armed mode
      */
-    ARMED(I18n.format("hbwhelper.dream.armed"));
+    ARMED("hbwhelper.dream.armed");
 
     /**
-     * Cache for better performance when querying a constant of Dream mode
-     * games using its display name and when getting the set of display names of
-     * every constant
+     * Translate key for the name of this Dream mode game
      */
-    private static final Map<String, DreamMode> DISPLAY_NAMES_MAP;
-
-    /**
-     * Unmodifiable {@link Collection} containing display names of every
-     * constants of this enumeration, in the order their corresponding constants
-     * are declared
-     */
-    private static final Collection<String> VALUE_NAMES;
-
-    static {
-        DISPLAY_NAMES_MAP = Maps.newLinkedHashMapWithExpectedSize(
-                DreamMode.values().length);
-        for (DreamMode dreamMode : DreamMode.values()) {
-            DISPLAY_NAMES_MAP.put(dreamMode.displayName, dreamMode);
-        }
-        VALUE_NAMES = Collections.unmodifiableCollection(
-                DISPLAY_NAMES_MAP.keySet());
-    }
-
-    /**
-     * Name of this Dream mode game displayed in the client
-     */
-    private final String displayName;
+    private final String translateKey;
 
     /**
      * Constructs a new constant of Bed Wars Dream mode games.
      *
-     * @param displayName the name of this Dream mode game displayed in the
-     *         client
+     * @param translateKey translate key for the name of this Dream mode game
+     * @throws NullPointerException if {@code translateKey == null}
      */
-    DreamMode(String displayName) {
-        this.displayName = displayName;
+    DreamMode(String translateKey) {
+        this.translateKey =
+                Objects.requireNonNull(translateKey, "translateKey");
     }
 
     /**
-     * Returns an unmodifiable {@link Collection} containing display names of
-     * every constants of this enumeration whose iteration order is the same as
-     * the order their corresponding constants are declared.
+     * Returns the translate key for the name of this Dream mode game.
      *
-     * @return an unmodifiable {@code Collection} containing display names of
-     *         every constants of this enumeration whose iteration order is the
-     *         same as the order their corresponding constants are declared
+     * @return the translate key for the name of this Dream mode game
      */
-    public static Collection<String> displayNames() {
-        return VALUE_NAMES;
-    }
-
-    /**
-     * Returns an {@link Optional} wrapping the constant of this enumeration
-     * with the specified display name, or an empty {@code Optional} if there is
-     * no such constant with the specified name.
-     * <p>
-     * An empty {@code Optional} will also be returned if the
-     * {@code displayName} argument is {@code null}.
-     *
-     * @param displayName the display name of the constant to return
-     * @return an {@code Optional} wrapping the constant of this enumeration
-     *         with the specified display name, or an empty {@code Optional} if
-     *         there is no such constant with the specified name or
-     *         {@code displayName == null}
-     */
-    public static Optional<DreamMode> valueOfDisplayName(String displayName) {
-        if (displayName == null) {
-            return Optional.empty();
-        }
-        return Optional.ofNullable(DISPLAY_NAMES_MAP.get(displayName));
-    }
-
-    /**
-     * Returns the name of this Dream mode game displayed in the client.
-     *
-     * @return the name of this Dream mode game displayed in the client
-     */
-    public String toDisplayName() {
-        return displayName;
+    public String getTranslateKey() {
+        return this.translateKey;
     }
 }
