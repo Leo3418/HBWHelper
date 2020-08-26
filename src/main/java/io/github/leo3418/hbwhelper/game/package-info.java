6df19113cffe0e14d325bd26c5a1cdb53d7eed94
Leo <Leo3418@users.noreply.github.com>
2020-08-25 22:46:21 -0700

    Update copyright notices for additional permission

--- src/main/java/io/github/leo3418/hbwhelper/game/package-info.java
+++ src/main/java/io/github/leo3418/hbwhelper/game/package-info.java
@@ -4,17 +4,23 @@
  * This file is part of Hypixel Bed Wars Helper (HBW Helper).
  *
  * HBW Helper is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License (GPL) as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * HBW Helper is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Under section 7 of GPL version 3, you are granted additional
+ * permissions described in the HBW Helper MC Exception.
+ *
+ * You should have received a copy of the GNU GPL and a copy of the
+ * HBW Helper MC Exception along with this program's source code; see
+ * the files LICENSE.txt and LICENSE-MCE.txt respectively.  If not, see
+ * <http://www.gnu.org/licenses/> and
+ * <https://github.com/Leo3418/HBWHelper>.
  */
 
 /**
